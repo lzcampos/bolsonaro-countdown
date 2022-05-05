abaec21bd3316e0470234cf2d3bdb0ce00bf8778
Luiz Damasceno <luizedc1@gmail.com>
2022-05-04 21:28:43 -0300

    Added mandate end countdown.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import bolsologo from './img/bolsonaro-cartoon.jpg';
+import twitterLogo from './img/twitter-logo.png';
 import './App.css';
+
+function getDaysToMandateEnd() {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  let diff = (now.valueOf() - start.valueOf()) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay);
+  return 365 - day
+}
+
+function getTweetText() {
+  let url = "https://twitter.com/intent/tweet?text="
+  let daysToMandateEnd = getDaysToMandateEnd()
+  url += `Faltam ${daysToMandateEnd} dias para o fim do governo Bolsonaro.`
+  return url
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={bolsologo} width="200px" height="200px" className="App-logo" alt="bolsonaro" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Faltam <span className="Days-Span">{getDaysToMandateEnd()}</span> dias para o fim do governo Bolsonaro.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Twitter-Container">
+          <a href={getTweetText()}
+            target="_blank"
+          >
+            <img src={twitterLogo} className="Twitter-Logo" alt="" />
+          </a>
+          <p>Poste isso no twitter</p>
+        </div>
       </header>
     </div>
   );
